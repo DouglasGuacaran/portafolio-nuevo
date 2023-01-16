0f876df060e2db0c8aa59015eafc9f02fe714ca7
DouglasGuacaran <douglasguacaran@gmail.com>
2023-01-16 12:08:53 -0300

    Cambios en el head

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="flex body">
     <div class="section1" id="section1">
       <div class="section1-blur">
-        <h1 class="flex text-title">Desarrollador Full Stack Douglas Guacarán</h1>
+        <h1 class="flex text-title">Douglas Guacarán</h1>
         <span class="text-subtitle"
           >He querido reorientar mi vida profesional hacia el desarrollo, y este sitio muestra un poco de lo que he podido alcanzar hasta el momento. He realizado muchas cosas diferentes durante este camino, cada proyecto que acepto lo abordo con el mayor compromiso, entusiasmo y pasión, tómate un tiempo para conocerlos. Espero que los encuentres interesante y que durante este recorrido sientas un poco de lo que hay dentro de mi y que resuene dentro de ti.</span
         >
